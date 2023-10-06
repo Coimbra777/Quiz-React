ffc130563d41faaf85be71a03e93ba766b63cbbc
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2023-10-05 22:13:01 -0300

    funcionalidades

--- src/Form/Radio.jsx
+++ src/Form/Radio.jsx
@@ -1,9 +1,35 @@
-const Radio = () => {
+import "../App.css";
+import PropTypes from "prop-types";
+
+const Radio = ({ pergunta, options, onChange, value, id, active }) => {
+  if (!active) return false;
+
   return (
     <fieldset>
-      <legend>Nome da pergunta</legend>
+      <legend>{pergunta}</legend>
+      {options.map((option) => (
+        <label key={option}>
+          <input
+            type="radio"
+            id={id}
+            checked={value === option}
+            value={option}
+            onChange={onChange}
+          />
+          {option}
+        </label>
+      ))}
     </fieldset>
   );
+};
+
+Radio.propTypes = {
+  pergunta: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  active: PropTypes.bool.isRequired,
 };
 
 export default Radio;
